d6c4c545daf305c2f7060b39f17a921d72226aba
Tampoka <goal.thanks@gmail.com>
2021-09-04 21:58:35 +0900

    added map for rendering tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TaskPropsType, Todolist} from "./Todolist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let task1:Array<TaskPropsType>=[
+        {id:1,title:"CSS",isDone:true},
+        {id:2,title:"JS",isDone:true},
+        {id:3,title:"React",isDone:false},
+    ]
+    let task2:Array<TaskPropsType>=[
+        {id:1,title:"Broccoli",isDone:true},
+        {id:2,title:"Juice",isDone:false},
+        {id:3,title:"Bread",isDone:false},
+        {id:4,title:"Milk",isDone:true},
+        {id:5,title:"Mayonnaise",isDone:false}
+    ]
+    let task3:Array<TaskPropsType>=[
+        {id:1,title:"Terminator",isDone:true},
+        {id:2,title:"Gentlemens of fortune",isDone:false},
+        {id:3,title:"Avatar",isDone:false},
+    ]
+    return (
+        <div className="App">
+            <Todolist title={"What to learn"} tasks={task1}/>
+            <Todolist title={"What to buy"} tasks={task2}/>
+            <Todolist title={"What to watch"} tasks={task3}/>
+        </div>
+    );
 }
 
 export default App;
