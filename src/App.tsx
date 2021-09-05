c6c0b88c76434c3e7f6cc91cba1d21f705a24b4d
Tampoka <goal.thanks@gmail.com>
2021-09-05 11:49:24 +0900

    left one Todo to simplify next steps

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,23 @@ function App() {
         {id:2,title:"JS",isDone:true},
         // {id:3,title:"React",isDone:false},
     ]
-    let task2:Array<TaskPropsType>=[
-        {id:1,title:"Broccoli",isDone:true},
-        {id:2,title:"Juice",isDone:false},
-        {id:3,title:"Bread",isDone:false},
-        {id:4,title:"Milk",isDone:true},
-        {id:5,title:"Mayonnaise",isDone:false}
-    ]
-    let task3:Array<TaskPropsType>=[
-        {id:1,title:"Terminator",isDone:true},
-        {id:2,title:"Gentlemens of fortune",isDone:false},
-        {id:3,title:"Avatar",isDone:false},
-    ]
+    // let task2:Array<TaskPropsType>=[
+    //     {id:1,title:"Broccoli",isDone:true},
+    //     {id:2,title:"Juice",isDone:false},
+    //     {id:3,title:"Bread",isDone:false},
+    //     {id:4,title:"Milk",isDone:true},
+    //     {id:5,title:"Mayonnaise",isDone:false}
+    // ]
+    // let task3:Array<TaskPropsType>=[
+    //     {id:1,title:"Terminator",isDone:true},
+    //     {id:2,title:"Gentlemens of fortune",isDone:false},
+    //     {id:3,title:"Avatar",isDone:false},
+    // ]
     return (
         <div className="App">
             <Todolist title={"What to learn"} tasks={task1}/>
-            <Todolist title={"What to buy"} tasks={task2}/>
-            <Todolist title={"What to watch"} tasks={task3}/>
+            {/*<Todolist title={"What to buy"} tasks={task2}/>*/}
+            {/*<Todolist title={"What to watch"} tasks={task3}/>*/}
         </div>
     );
 }
