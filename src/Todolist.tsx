6334531964c58297816886cbb6e25122b9085c79
Tampoka <goal.thanks@gmail.com>
2021-09-14 21:36:44 +0900

    copied callbacks inside AddItemForm

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -91,7 +91,34 @@ export function Todolist(props: PropsType) {
 
 type addItemFormPropsType = {}
 
-function addItemForm(props: addItemFormPropsType) {
+function AddItemForm(props: addItemFormPropsType) {
+    const [newTaskTitle, setNewTaskTitle] = useState("")
+    const [error, setError] = useState<string | null>(null)
+
+    const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitle((e.currentTarget.value))
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (e.key === "Enter") {
+            if (newTaskTitle.trim() !== ""
+                && newTaskTitle !== "hell") {
+                props.addTask(newTaskTitle, props.id)
+                setNewTaskTitle("")
+            } else {
+                setError("Title is required")
+            }
+        }
+    }
+    const addTask = () => {
+        if (newTaskTitle.trim() !== ""
+            && newTaskTitle !== "hell") {
+            props.addTask(newTaskTitle, props.id)
+            setNewTaskTitle("")
+        } else {
+            setError("Title is required")
+        }
+    }
     return <div>
         <input value={newTaskTitle}
                onChange={onNewTitleChangeHandler}
