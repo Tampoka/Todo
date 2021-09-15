9ad1a3f46744332161eb2439d03bd0242ae813ee
Tampoka <goal.thanks@gmail.com>
2021-09-15 17:21:04 +0900

    added onChangeTitleHandler

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,23 +1,30 @@
-import React, {KeyboardEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 
 type EditableSpanPropsType = {
     title: string
 }
 
 export function EditableSpan(props: EditableSpanPropsType) {
-    let [editMode, setEditMode]=useState(false)
+    let [editMode, setEditMode] = useState(false)
+    let [title, setTitle] = useState("")
 
-    const activateEditMode=()=> setEditMode(true)
+    const activateEditMode = () => setEditMode(true)
 
-    const activateViewMode=()=> setEditMode(false)
+    const activateViewMode = () => setEditMode(false)
 
     const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if(e.key==="Enter"){
+        if (e.key === "Enter") {
             setEditMode(false)
         }
     }
+
+    const onChangeTitleHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    setTitle(e.currentTarget.value)
+    }
     return editMode
-        ?<input value={props.title}  onBlur={activateViewMode}
-        onKeyPress={onKeyPressHandler} autoFocus/>
-    :<span onDoubleClick={activateEditMode}>{props.title}</span>
+        ? <input value={title}
+                 onBlur={activateViewMode}
+                 onKeyPress={onKeyPressHandler}
+                 onChange={onChangeTitleHandler} autoFocus/>
+        : <span onDoubleClick={activateEditMode}>{props.title}</span>
 }
