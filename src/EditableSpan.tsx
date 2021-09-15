a7a8eaff74c3bc0dae1e0fa8a3e8d01fbebcee5f
Tampoka <goal.thanks@gmail.com>
2021-09-15 16:57:23 +0900

    created activateEditMode, activateViewMode

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 type EditableSpanPropsType = {
     title: string
 }
 
 export function EditableSpan(props: EditableSpanPropsType) {
-    return <span>{props.title}</span>
+    let [editMode, setEditMode]=useState(false)
+
+    const activateEditMode=()=> setEditMode(true)
+
+    const activateViewMode=()=> setEditMode(false)
+
+
+
+    return editMode
+        ?<input value={props.title}/>
+    :<span onDoubleClick={activateEditMode}
+        onBlur={activateViewMode}>{props.title}</span>
 }
