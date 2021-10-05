bcecb9ddd4459ee9c5f69561b9a9232e84180717
Tampoka <goal.thanks@gmail.com>
2021-10-05 13:19:45 +0900

    added AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {TaskPropsType, Todolist} from "./Todolist";
 import {v1} from "uuid";
 import {AddItemForm} from "./AddItemForm";
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {Menu} from "@material-ui/icons";
 
 
 export function Counter() {
@@ -128,6 +130,18 @@ function App() {
 
     return (
         <div className="App">
+            <AppBar position={"static"}>
+                <Toolbar>
+                    <IconButton edge={"start"} color={"inherit"} aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color={"inherit"}>Login</Button>
+                </Toolbar>
+            </AppBar>
+
             <AddItemForm addItem={addTodolist}/>
             {
                 todolists.map(tl => {
