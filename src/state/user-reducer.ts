69df78631fbcd7923534c5a050d16e83ba0d1c1c
Tampoka <goal.thanks@gmail.com>
2021-10-10 23:09:50 +0900

    made userReduser passed test

--- src/state/user-reducer.ts
+++ src/state/user-reducer.ts
@@ -18,6 +18,11 @@ export const userReducer = (state: StateType, action: ActionType) => {
         case 'INCREMENT-CHILDREN-COUNT':
             return {...state,
             childrenCount:state.childrenCount+1}
+        case 'CHANGE-NAME':
+            return {
+                ...state,
+                name:action.newName
+            }
         default:
             throw new Error("I don't understand this type")
     }
