bec7bf798710702321b22be442cb8bb2cd149782
Tampoka <goal.thanks@gmail.com>
2021-10-11 20:01:49 +0900

    added test for equal ids

---
+++ src/state/todolist-task-reducer.test.ts
@@ -0,0 +1,22 @@
+import {TasksStateType, TodolistType} from "../App";
+import {AddTodolistAC, todolistReducer} from "./todolist-reducer";
+import {taskReducer} from "./tasks-reducer";
+
+test('ids should be equals', () => {
+    const startTaskState: TasksStateType = {}
+    const startTodolistState: Array<TodolistType> = []
+
+    const action = AddTodolistAC("new todolist")
+
+    const endTasksState =taskReducer(startTaskState,action)
+    const endTodolistsState =todolistReducer(startTodolistState,action)
+
+    const keys=Object.keys(endTasksState)
+    const idFromTasks=keys[0]
+    const idFromTodolists=endTodolistsState[0].id
+
+    expect(idFromTasks).toBe(action.todolistId)
+    expect(idFromTodolists).toBe(action.todolistId)
+
+
+})
