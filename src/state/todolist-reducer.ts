5b53c3194ffc48d33ec032f2da167ebe066f1ab9
Tampoka <goal.thanks@gmail.com>
2021-10-11 10:18:40 +0900

    added 'REMOVE-TODOLIST' case

--- src/state/todolist-reducer.ts
+++ src/state/todolist-reducer.ts
@@ -7,8 +7,9 @@ type ActionType = {
 
 export const todolistReducer = (state: Array<TodolistType>, action: ActionType) => {
     switch (action.type) {
-        case 'XXX':
-            return state
+        case 'REMOVE-TODOLIST':
+    let newState= state.filter(tl => tl.id !== action.id)
+            return newState
         default:
             throw new Error("I don't understand this type")
     }
