5f0bfb08815e89a5663299d2c617722309aed55f
Tampoka <goal.thanks@gmail.com>
2021-10-26 12:57:53 +0900

    added  redux-devtools-extension

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,20 @@
 import {combineReducers, createStore} from "redux";
 import {taskReducer} from "./tasks-reducer";
 import {todolistReducer} from "./todolist-reducer";
+import {composeWithDevTools} from "redux-devtools-extension";
+
 
 const rootReducer = combineReducers({
     tasks: taskReducer,
     todolists: todolistReducer
 })
 
-export const store=createStore(rootReducer)
+const composeEnhancers = composeWithDevTools
+
+export const store=createStore(rootReducer,composeEnhancers())
 
 export type AppRootStateType=ReturnType<typeof rootReducer>
+
 
 // @ts-ignore
 window.store=store
