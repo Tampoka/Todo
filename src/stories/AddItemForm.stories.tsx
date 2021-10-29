86fafb9dd697ff5a0163149a0be7955ccc1e5a7c
Tampoka <goal.thanks@gmail.com>
2021-10-29 22:55:17 +0900

    created story for AddItemForm

---
+++ src/stories/AddItemForm.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {AddItemForm} from "../AddItemForm";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Todolist/AddItemForm',
+    component: AddItemForm,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        onclick: { description: 'Button inside form clicked' },
+    },
+} as ComponentMeta<typeof AddItemForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const Primary = Template.bind({});
+
