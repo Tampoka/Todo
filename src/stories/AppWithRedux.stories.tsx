b679b8b93c56119d5f3aa9529af8e2f17fde7f8e
Tampoka <goal.thanks@gmail.com>
2021-10-30 15:56:36 +0900

    created storyBookStore

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,15 +1,15 @@
-import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {ComponentMeta, Story} from "@storybook/react";
 import React from "react";
 import AppWithRedux from "../AppWithRedux";
+import {ReduxStoreProviderDecorator} from "./decorators/ReduxStoreProviderDecorator";
 
 export default {
     title: 'Todolist/AppWithRedux',
     component: AppWithRedux,
-    argTypes: {},
+    decorators:[ReduxStoreProviderDecorator]
 } as ComponentMeta<typeof AppWithRedux>;
 
 
-const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux {...args} />;
+const Template: Story=()=><AppWithRedux/>;
 export const AppWithReduxExample=Template.bind({})
-AppWithReduxExample.args={}
 
--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,6 +1,38 @@
-import {store} from "../../state/store";
+import {AppRootStateType} from "../../state/store";
 import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+import {v1} from "uuid";
+import {taskReducer} from "../../state/tasks-reducer";
+import {todolistReducer} from "../../state/todolist-reducer";
 
-export const ReduxStoreProviderDecorator=(storyFn:any)=>{
-    return <Provider store={store}>{storyFn()}</Provider>
-}
+
+const rootReducer = combineReducers({
+    tasks: taskReducer,
+    todolists: todolistReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        {id: "todolistId1", title: "What to learn", filter: "all"},
+        {id: "todolistId2", title: "What to buy", filter: "all"}
+    ] ,
+    tasks: {
+        ["todolistId1"]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true}
+        ],
+        ["todolistId2"]: [
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "React Book", isDone: true}
+        ]
+    }
+};
+
+export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType);
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => (
+    <Provider
+        store={storyBookStore}>{storyFn()}
+    </Provider>)
+
+
