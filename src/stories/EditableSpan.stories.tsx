ec0ebdfade930272563ebc7fe20ca4e964d92d2e
Tampoka <goal.thanks@gmail.com>
2021-10-30 14:38:28 +0900

    story for EditableSpan

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,24 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import React from "react";
+import {EditableSpan} from "../EditableSpan";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'Todolist/EditableSpan',
+    component: EditableSpan,
+    argTypes: {
+        onChange: { description: 'Value changed' },
+        value:{
+            defaultValue:'HTML',
+            description:'Start value'
+        }
+    },
+} as ComponentMeta<typeof EditableSpan>;
+
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args} />;
+
+export const EditableSpanExample = Template.bind({});
+EditableSpanExample.args = {
+   onChange:action('Value changed')
+};
+
