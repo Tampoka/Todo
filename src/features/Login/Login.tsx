c343956de48000b641948f3009d3f36a4dd928e1
Tampoka <goal.thanks@gmail.com>
2021-12-26 23:50:52 +0900

    Add useFormik.

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from "@mui/material";
+import {useFormik} from "formik";
 
 export const Login = () => {
+
+    const formik=useFormik({
+        initialValues:{
+            email:'',
+            password:'',
+            rememberMe:false,
+        },
+        onSubmit:values=>{
+            alert(JSON.stringify(values))
+        }
+    })
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
-            <FormControl>
+            <form>
+                <FormControl>
                 <FormLabel>
                     <p>To log in get registered
                         <a href={'https://social-network.samuraijs.com/'}
@@ -26,6 +39,7 @@ export const Login = () => {
                     </Button>
                 </FormGroup>
             </FormControl>
+            </form>
         </Grid>
     </Grid>
 
