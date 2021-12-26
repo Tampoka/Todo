6d56834d01072af6325d621e53e86d00a6cef1b2
Tampoka <goal.thanks@gmail.com>
2021-12-27 00:03:25 +0900

    Add checked.

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -4,13 +4,13 @@ import {useFormik} from "formik";
 
 export const Login = () => {
 
-    const formik=useFormik({
-        initialValues:{
-            email:'',
-            password:'',
-            rememberMe:false,
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false,
         },
-        onSubmit:values=>{
+        onSubmit: values => {
             alert(JSON.stringify(values))
         }
     })
@@ -18,27 +18,29 @@ export const Login = () => {
         <Grid item justifyContent={'center'}>
             <form onSubmit={formik.handleSubmit}>
                 <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href={'https://social-network.samuraijs.com/'}
-                           target={'_blank'}> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                    <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}/>
-                    <TextField type="password" label="Password"
-                               margin="normal" {...formik.getFieldProps('password')}/>
-                    <FormControlLabel label={'Remember me'}
-                                      control={<Checkbox name="rememberMe"/>} {...formik.getFieldProps('rememberMe')}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
-            </FormControl>
+                    <FormLabel>
+                        <p>To log in get registered
+                            <a href={'https://social-network.samuraijs.com/'}
+                               target={'_blank'}> here
+                            </a>
+                        </p>
+                        <p>or use common test account credentials:</p>
+                        <p>Email: free@samuraijs.com</p>
+                        <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+                        <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}/>
+                        <TextField type="password" label="Password"
+                                   margin="normal" {...formik.getFieldProps('password')}/>
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox
+                                              name="rememberMe"/>} {...formik.getFieldProps('rememberMe')}
+                                          checked={formik.values.rememberMe}/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </FormControl>
             </form>
         </Grid>
     </Grid>
