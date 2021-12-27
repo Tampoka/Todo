e85249095461f1dcda6ea4d0e71fdcee3e49a0fb
Tampoka <goal.thanks@gmail.com>
2021-12-27 21:07:58 +0900

    Add isInitialized.

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -11,109 +11,110 @@ import {appReducer} from "../../redux/app-reducer";
 const rootReducer = combineReducers({
     tasks: taskReducer,
     todolists: todolistsReducer,
-    app:appReducer
+    app: appReducer
 })
 
 const initialGlobalState = {
     todolists: [
-            {
-                id:"todolistId1", title: "What to learn", filter: "all", addedDate: '',
-                order: 0,entityStatus:'idle'
-            },
-            {
-                id: "todolistId2", title: "What to buy", filter: "all", addedDate: '',
-                order: 0,entityStatus:'loading'
-            }
-        ],
+        {
+            id: "todolistId1", title: "What to learn", filter: "all", addedDate: '',
+            order: 0, entityStatus: 'idle'
+        },
+        {
+            id: "todolistId2", title: "What to buy", filter: "all", addedDate: '',
+            order: 0, entityStatus: 'loading'
+        }
+    ],
     tasks:
         {
-        "todolistId1": [
-            {
-                id: v1(), title: "CSS", status: TaskStatuses.Completed, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId1",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "JS", status: TaskStatuses.Completed, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId1",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "React", status: TaskStatuses.New, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId1",
-                order: 0,
-                addedDate: ''
-            }
-        ],
-        "todolistId2": [
-            {
-                id: v1(), title: "Broccoli", status: TaskStatuses.Completed, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId2",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "Juice", status: TaskStatuses.New, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId2",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "Bread", status: TaskStatuses.New, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId2",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "Milk", status: TaskStatuses.Completed, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId2",
-                order: 0,
-                addedDate: ''
-            },
-            {
-                id: v1(), title: "Mayonnaise", status: TaskStatuses.New, description: 'new task',
-                priority: 0,
-                startDate: '',
-                deadline: '',
-                todoListId: "todolistId2",
-                order: 0,
-                addedDate: ''
-            }
-        ]
+            "todolistId1": [
+                {
+                    id: v1(), title: "CSS", status: TaskStatuses.Completed, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId1",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "JS", status: TaskStatuses.Completed, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId1",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "React", status: TaskStatuses.New, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId1",
+                    order: 0,
+                    addedDate: ''
+                }
+            ],
+            "todolistId2": [
+                {
+                    id: v1(), title: "Broccoli", status: TaskStatuses.Completed, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId2",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "Juice", status: TaskStatuses.New, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId2",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "Bread", status: TaskStatuses.New, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId2",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "Milk", status: TaskStatuses.Completed, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId2",
+                    order: 0,
+                    addedDate: ''
+                },
+                {
+                    id: v1(), title: "Mayonnaise", status: TaskStatuses.New, description: 'new task',
+                    priority: 0,
+                    startDate: '',
+                    deadline: '',
+                    todoListId: "todolistId2",
+                    order: 0,
+                    addedDate: ''
+                }
+            ]
+        },
+    app: {
+        status: 'idle',
+        error: null,
+        isInitialized: true,
     },
-   app:{
-        status:'idle',
-       error:null,
-   },
-    auth:{
+    auth: {
         isLoggedIn: false
     }
 };
 
-export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType,applyMiddleware(thunk));
+export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType, applyMiddleware(thunk));
 
 export const ReduxStoreProviderDecorator = (storyFn: any) => {
     return <Provider
