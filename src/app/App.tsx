265e4bcc8044d30ac9644829c9dd8eaec99f5599
Tampoka <goal.thanks@gmail.com>
2021-12-27 23:03:02 +0900

    Add useEffect for initializeAppTC.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Box from '@mui/material/Box';
 import {Menu} from "@mui/icons-material";
@@ -16,12 +16,11 @@ import {
 import themeOptions from "../common/color-sheme";
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
 import {ErrorSnackBar} from "../components/ErrorSnackBar/ErrorSnackBar";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../redux/store";
-import {RequestStatusType} from "../redux/app-reducer";
+import {initializeAppTC, RequestStatusType} from "../redux/app-reducer";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "../features/Login/Login";
-import {green} from "@mui/material/colors";
 
 type PropsType = {
     demo?: boolean
@@ -30,6 +29,11 @@ type PropsType = {
 function App({demo = false}: PropsType) {
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
     const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(initializeAppTC())
+    },[dispatch])
 
     if (!isInitialized) {
         return <CircularProgress size={40}
@@ -39,43 +43,44 @@ function App({demo = false}: PropsType) {
                                      top: '50%',
                                      left: '50%',
                                      marginTop: '-20px',
-                                     marginLeft: '-20px',}}/>
-                                 }
+                                     marginLeft: '-20px',
+                                 }}/>
+    }
 
-                                     return (
-                                     <BrowserRouter>
-                                     <ThemeProvider theme={themeOptions}>
-                                     <div className="App">
-                                     <Box sx={{flexGrow: 1}}>
-                                 {/* eslint-disable-next-line react/jsx-no-undef */}
-                                     <AppBar position={"static"}>
-                                     <Toolbar>
-                                     <IconButton edge={"start"} color={"inherit"} aria-label="menu" size="large"
-                                     sx={{mr: 2}}>
-                                     <Menu/>
-                                     </IconButton>
-                                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                                     News
-                                     </Typography>
-                                     <Button color={"inherit"}>Login
-                                 {/*<Link to="/login">{isLoggedIn ? 'Log out' : 'Login'}</Link>*/}
-                                     </Button>
-                                     </Toolbar>
-                                 {status === 'loading' && <LinearProgress color='secondary'/>}
-                                     </AppBar>
-                                     <ErrorSnackBar/>
-                                     </Box>
-                                     <Container fixed>
-                                     <Routes>
-                                     <Route path="*" element={<TodolistsList demo={demo}/>}/>
-                                     <Route path="/login" element={<Login/>}/>
-                                     </Routes>
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={themeOptions}>
+                <div className="App">
+                    <Box sx={{flexGrow: 1}}>
+                        {/* eslint-disable-next-line react/jsx-no-undef */}
+                        <AppBar position={"static"}>
+                            <Toolbar>
+                                <IconButton edge={"start"} color={"inherit"} aria-label="menu" size="large"
+                                            sx={{mr: 2}}>
+                                    <Menu/>
+                                </IconButton>
+                                <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                                    News
+                                </Typography>
+                                <Button color={"inherit"}>Login
+                                    {/*<Link to="/login">{isLoggedIn ? 'Log out' : 'Login'}</Link>*/}
+                                </Button>
+                            </Toolbar>
+                            {status === 'loading' && <LinearProgress color='secondary'/>}
+                        </AppBar>
+                        <ErrorSnackBar/>
+                    </Box>
+                    <Container fixed>
+                        <Routes>
+                            <Route path="*" element={<TodolistsList demo={demo}/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                        </Routes>
 
-                                     </Container>
-                                     </div>
-                                     </ThemeProvider>
-                                     </BrowserRouter>
-                                     );
-                                 }
+                    </Container>
+                </div>
+            </ThemeProvider>
+        </BrowserRouter>
+    );
+}
 
-                                 export default App;
+export default App;
