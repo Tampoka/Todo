b5a5eee82eda04a55e5262265f95f63fde1fdedc
Tampoka <goal.thanks@gmail.com>
2021-12-27 10:33:07 +0900

    Add validate.

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -5,6 +5,18 @@ import {useFormik} from "formik";
 export const Login = () => {
 
     const formik = useFormik({
+        validate:(values) =>{
+            if(!values.email){
+                return {
+                    email:'Email is required'
+                }
+            }
+            if(!values.password){
+                return {
+                    password:'Password is required'
+                }
+            }
+        } ,
         initialValues: {
             email: '',
             password: '',
@@ -30,8 +42,10 @@ export const Login = () => {
                     </FormLabel>
                     <FormGroup>
                         <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}/>
+                        {formik.errors.email?<div>{formik.errors.email}</div>:null}
                         <TextField type="password" label="Password"
                                    margin="normal" {...formik.getFieldProps('password')}/>
+                        {formik.errors.password?<div>{formik.errors.password}</div>:null}
                         <FormControlLabel label={'Remember me'}
                                           control={<Checkbox
                                               name="rememberMe"/>} {...formik.getFieldProps('rememberMe')}
