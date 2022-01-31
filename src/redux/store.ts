0d95c8eb9081601a2c0853d3869c204451dfbf2d
Tampoka <goal.thanks@gmail.com>
2022-02-01 00:12:51 +0900

    Add redux-logger.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers} from "redux";
 import {taskReducer} from "./tasks-reducer";
 import {todolistsReducer} from "./todolists-reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import {appReducer} from "./app-reducer";
 import {authReducer} from "./auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
 
 
 const rootReducer = combineReducers({
@@ -17,8 +19,16 @@ const rootReducer = combineReducers({
 const middlewareEnhancer = applyMiddleware(thunkMiddleware)
 const composeEnhancers = composeWithDevTools(middlewareEnhancer)
 
-export const store = createStore(rootReducer, composeEnhancers)
-
+// export const store = createStore(rootReducer, composeEnhancers)
+export const store=configureStore({
+    reducer:rootReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware()
+            .prepend(
+                thunkMiddleware)
+            // prepend and concat calls can be chained
+            .concat(logger)
+})
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
 //export const useAppDispatch = () => useDispatch<AppDispatch>()
