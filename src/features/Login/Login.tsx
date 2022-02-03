29f0dda21b2f53884dcd4afbc4c4d4d58711445e
Tampoka <goal.thanks@gmail.com>
2022-02-03 16:29:25 +0900

    Add setFieldError.

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,17 +1,17 @@
 import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField} from "@mui/material";
 import {FormikHelpers, useFormik} from "formik";
-import {useDispatch, useSelector} from "react-redux";
-import {loginTC} from "../../redux/auth-reducer";
-import {AppRootStateType} from "../../redux/store";
+import {useSelector} from "react-redux";
+import {AppRootStateType, useAppDispatch} from "../../redux/store";
 import {Navigate} from 'react-router-dom';
+import {loginTC} from "../../redux/auth-reducer";
 
-type FormValuesType={
-    email:string
-    password:string
-    rememberMe:boolean
+type FormValuesType = {
+    email: string
+    password: string
+    rememberMe: boolean
 }
 export const Login = () => {
-    const dispatch = useDispatch()
+    const dispatch = useAppDispatch()
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
 
     const formik = useFormik({
@@ -32,8 +32,17 @@ export const Login = () => {
             password: '',
             rememberMe: false,
         },
-        onSubmit: async(values,formikHelpers:FormikHelpers<FormValuesType>) => {
-            const res=await dispatch(loginTC(values))
+        onSubmit: async (values: FormValuesType, formikHelpers: FormikHelpers<FormValuesType>) => {
+            // @ts-ignore
+            const action = await dispatch(loginTC(values))
+            if (loginTC.rejected.match(action)) {
+                if (action.payload?.fieldsErrors?.length) {
+                    const error = action.payload.fieldsErrors[0]
+                    formikHelpers.setFieldError(error.field, error.error)
+                }
+            }
+            debugger
+
         }
     })
     if (isLoggedIn) {
@@ -55,10 +64,12 @@ export const Login = () => {
                         <p>Password: free</p>
                     </FormLabel>
                     <FormGroup>
-                        <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}  autoComplete="user-email"/>
+                        <TextField label="Email" margin="normal" {...formik.getFieldProps('email')}
+                                   autoComplete="user-email"/>
                         {formik.errors.email ? <div>{formik.errors.email}</div> : null}
                         <TextField type="password" label="Password"
-                                   margin="normal" {...formik.getFieldProps('password')}  autoComplete="current-password"/>
+                                   margin="normal" {...formik.getFieldProps('password')}
+                                   autoComplete="current-password"/>
                         {formik.errors.password ? <div>{formik.errors.password}</div> : null}
                         <FormControlLabel label={'Remember me'}
                                           control={<Checkbox
